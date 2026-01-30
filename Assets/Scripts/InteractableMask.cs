2d46f8541c02d25d91eec46a3334189acdd1138b
Lucas Nascimento Pereira <lucasnpereira@ua.pt>
2026-01-30 21:59:11 +0000

    mask

--- Assets/Scripts/InteractableMask.cs
+++ Assets/Scripts/InteractableMask.cs
@@ -2,8 +2,21 @@ using UnityEngine;
 
 public class InteractableMask : MonoBehaviour, IInteractable
 {
+    public void DeregisterInteractable()
+    {
+        GameManager.Instance.RegisterInteractable(null);
+    }
+
     public void Interact()
     {
         //Unlock mask in player or something
+        Destroy(gameObject);
     }
+
+    public void RegisterInteractable()
+    {
+        GameManager.Instance.RegisterInteractable(gameObject);
+    }
+
+     
 }
