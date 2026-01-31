eb46f678b453b0709f59e443e8c9d3c362bac018
heitorwirmond <heitorfranciscowirmond@hotmail.com>
2026-01-31 11:28:37 +0000

    camera follow, and dash
    camera follow and dash

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
@@ -7,6 +8,10 @@ public class PlayerControls : MonoBehaviour
     public float speed = 0.5f;
     private Rigidbody2D rb;
     private Vector2 input;
+    private Vector2 lastMoveDirection;
+    [SerializeField] private float dashSpeed = 15f;
+    [SerializeField] private float dashTime = 0.2f;
+    private bool isDashing;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -15,13 +20,33 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isDashing) return;
+
         input.x = Input.GetAxisRaw("Horizontal");
         input.y = Input.GetAxisRaw("Vertical");
-
-        input.Normalize();
+        if(input != Vector2.zero)
+        {
+            lastMoveDirection = input.normalized;
+        }
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            StartCoroutine(ExecuteDash());
+        }
     }
     private void FixedUpdate()
     {
+        if (isDashing) return;
         rb.linearVelocity = input * speed;
+    }
+    private IEnumerator ExecuteDash()
+    {
+        isDashing = true;
+        if (lastMoveDirection == Vector2.zero) lastMoveDirection = Vector2.right;
+
+        rb.linearVelocity = lastMoveDirection * dashSpeed;
+
+        yield return new WaitForSeconds(dashTime);
+
+        isDashing = false;
     }
 }
